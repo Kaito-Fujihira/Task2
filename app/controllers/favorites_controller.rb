d1ce01ed0e3a973589d11b2183ce4951ce4153ac
KaitoFujihira <k.f.alamoana@gmail.com>
2021-01-18 00:16:41 +0000

    update favorited_by?

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,13 +2,17 @@ class FavoritesController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    favorite = @book.favorites.new(user_id: current_user.id)
-    favorite.save
+    unless @book.favorited_by?(current_user)
+      favorite = @book.favorites.new(user_id: current_user.id)
+      favorite.save
+    end
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
-    favorite.destroy
+    if @book.favarited_by?(current_user)
+      favorite = current_user.favorites.find_by(book_id: @book.id)
+      favorite.destroy
+    end
   end
 end
